cdd86dd96afd71a06857f3cea600add5a5155c94
Jorge Chaves <chavesuj@prosoft.cr>
2020-09-06 14:17:38 -0600

    se agrega scroll de productos

--- FoodDeliveryApp/ContentView.swift
+++ FoodDeliveryApp/ContentView.swift
@@ -12,6 +12,17 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Spacer()
+            ScrollView(.horizontal) {
+                          HStack(spacing: 10) {
+            ForEach(ProductData) { item in
+                ProductCardView(product: item)
+                            }
+                }
+            }
+            .padding(15)
+            .padding(.bottom, 15)
+            
+            Spacer()
             NearYouView()
             Spacer()
             HStack {
@@ -68,7 +79,7 @@ struct NearYouItemView: View {
                 Spacer()
                 Text("12: 00 am - 12:00 pm")
                     .font(.system(size: 10))
-                    .fontWeight(.thin)
+                    .fontWeight(.light)
                 Spacer()
                 HStack {
                     Image(systemName: "star.fill")
@@ -90,9 +101,17 @@ struct NearYouItemView: View {
         }
         .frame(width: 210, height: 75, alignment: .leading)
         .background(Color("LightGray"))
-        .cornerRadius(8)
+        .cornerRadius(10)
     }
 }
+struct ProductRow: Identifiable{
+    var id = UUID()
+    var Name: String
+    var Image: String
+    var Description: String
+    var Value: String
+}
+let ProductData = [ProductRow(Name: "Pigeon Burger", Image: "burger1", Description: "American Burger with Extra Chesse", Value: "$5,99"), ProductRow(Name: "Toasty Burgers", Image: "burger2", Description: "Mexican Beef burger with chees", Value: "$7,99")]
 struct NearYouRow: Identifiable{
     var id = UUID()
     var CommerceName: String
@@ -126,5 +145,52 @@ struct NearYouView: View {
                 .padding(.bottom, 15)
             }
         }
+    }
+}
+
+struct ProductCardView: View {
+    var product: ProductRow
+    var body: some View {
+        VStack {
+            VStack {
+                Image(product.Image)
+                    .resizable()
+                    .frame(width: 150, height: 123, alignment: .center)
+                    .shadow(color: Color("YellowShadow"), radius: 35, x: 0, y: 15)
+                VStack(alignment: .leading) {
+                    Text(product.Name)
+                        .font(.system(size: 16))
+                        .fontWeight(.medium)
+                    Text(product.Description)
+                        .font(.system(size: 10))
+                        .fontWeight(.light)
+                        .padding(.top, 10)
+                    Text(product.Value)
+                        .font(.system(size: 16))
+                        .fontWeight(.medium)
+                        .padding(.top, 15)
+                }
+            }
+            .frame(width: 200, height: 293, alignment: .center)
+            .background(Color("GrayLight"))
+            .cornerRadius(15)
+            HStack {
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "chevron.right")
+                        .font(.system(size: 13))
+                        .frame(width: 30, height: 30, alignment: .center)
+                        
+                        .background(Color("Yellow"))
+                        .foregroundColor(Color.white)
+                        .clipShape(Circle())
+                        .offset(y: -45)
+                    
+                }
+                .padding()
+            }
+        }
+        
+        
     }
 }
