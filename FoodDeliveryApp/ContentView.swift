9ede380f58ad6c287d0e8ac69a6127c1b3d2a0fa
Jorge Chaves <chavesuj@prosoft.cr>
2020-09-05 22:36:24 -0600

    Initial Commit

---
+++ FoodDeliveryApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodDeliveryApp
+//
+//  Created by Jorge Eduardo Chaves Ulate on 9/5/20.
+//  Copyright © 2020 Jorge Eduardo Chaves Ulate. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
